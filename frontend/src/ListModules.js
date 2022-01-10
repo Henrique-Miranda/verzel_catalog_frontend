548cf764096a2679bfd62078663e9b014edcd734
Henrique Miranda <henrique@disroot.org>
2022-01-10 02:04:43 -0300

    Setup home page structure

--- frontend/src/ListModules.js
+++ frontend/src/ListModules.js
@@ -8,7 +8,7 @@ export default class ListModules extends react.Component{
                 'Content-Type': 'application/json'
             }
         }
-        config.headers['Authorization'] = 'Token 1030b79b9cd6b52d350310969231d0b2bc21f227'
+        config.headers['Authorization'] = 'Token bb30898927ed807c497d9766fda5ceb073f2cbb6'
         const url = 'http://localhost:8000/modules/'
         const response = await fetch(url, config);
         const data = await response.json();
@@ -19,9 +19,13 @@ export default class ListModules extends react.Component{
     render(){
         const all_modules = this.state.modules;
         return (
+            <div className="list_modules">
+            <h2>Módulos:</h2>
+            <p>Selecione o módulo para ver as aulas disponíveis:</p>
             <ul>
-                { all_modules.map(module => <button key={module.id}> {module.name} | Aulas diponiveis: {module.lesson_set.length} </button>)}
+                { all_modules.map(module => <button key={module.id}> {module.name} | Aulas diponiveis: {module.lesson_set.length} / {module.total_lessons.toString()} </button>)}
             </ul>
+            </div>
         )
     }
     
